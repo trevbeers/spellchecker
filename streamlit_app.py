b9aa3a92a061c54439af9901544a75e2aab22d23
trevbeers <trevor.beers@phrasee.co>
2021-09-24 08:42:18 +0100

    Create repo

---
+++ streamlit_app.py
@@ -0,0 +1,72 @@
+import re
+from io import StringIO
+import pandas as pd
+from spellchecker import SpellChecker
+import streamlit as st
+
+
+def check_spelling(files_dict):
+    spell = SpellChecker()
+
+    misspelled_line = []
+    misspelled_word = []
+    file_name = []
+
+    for name, contents in files_dict.items():
+        file_lines = contents.splitlines()
+
+        for line in file_lines:
+            if line.strip() and not line.startswith('#'):
+                og_line = line
+
+                if line.startswith(','):
+                    end = len(line)
+                    matches = re.finditer('[,][0-9]', line)
+                    for match in matches:
+                        end = match.start()
+                    line = line[1:end]
+
+                line = line.replace('...', '.')
+                line = line.replace('</', '')
+                line = line.replace(" '", ' ')
+                line = line.replace("' ", ' ')
+                line = re.sub('/[A-Z_]+}', '}', line)
+                line = re.sub('[~"^|{}<>*]|NoPUNCT|NoCAPS|PIPESYM', '', line)
+                line = re.sub(r'\[[0-9A-Za-z/;_]+\]', ' ', line)
+                line = line.replace('/', ' ')
+                line = re.sub(' +', ' ', line)
+
+                words = [w.strip() for w in re.split(r'[\s,.!?—()-:]', line)
+                         if w.strip() and not w.startswith('[') and w != '|' and '_' not in w]
+                words = words[1:]
+
+                misspelled = spell.unknown(words)
+                misspelled = [m for m in misspelled if any([ch.isalpha() for ch in list(m)]) and len(m) > 1]
+                if misspelled:
+                    misspelled_line += [og_line]
+                    misspelled_word += ['; '.join(misspelled)]
+                    file_name += [name]
+
+    df = pd.DataFrame()
+    df['File'] = file_name
+    df['Phrase'] = misspelled_line
+    df['Possible misspellings'] = misspelled_word
+
+    return df
+
+
+st.title("Spelling check")
+raw_files = st.file_uploader('Select syn and lex files', accept_multiple_files=True, key='files')
+files = {}
+for f in raw_files:
+    # To read file as bytes:
+    bytes_data = f.getvalue()
+    # To convert to a string based IO:
+    stringio = StringIO(f.getvalue().decode("utf-8"))
+    # To read file as string:
+    string_data = stringio.read()
+    files[f.name] = string_data
+if files:
+    if st.button('Check spelling'):
+        spelling_df = check_spelling(files)
+        st.dataframe(spelling_df)
